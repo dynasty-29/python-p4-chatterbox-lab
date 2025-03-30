c3d8fba9d307ee40d7b52e655a5d8ba61a419383
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-03-30 12:29:24 +0300

    frontend and backend with flask

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+# @app.route('/messages')
+# def messages():
+#     return ''
+
+
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return make_response(jsonify([msg.to_dict() for msg in messages]), 200)
+    
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'body' not in data or 'username' not in data:
+            return make_response(jsonify({"error": "Missing body or username"}), 400)
+        
+        new_message = Message(body=data['body'], username=data['username'])
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(jsonify(new_message.to_dict()), 201)
 
-@app.route('/messages/<int:id>')
+# @app.route('/messages/<int:id>')
+# def messages_by_id(id):
+#     return ''
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
-
+    message = Message.query.get(id)
+    if not message:
+        return make_response(jsonify({"error": "Message not found"}), 404)
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        if 'body' in data:
+            message.body = data['body']
+            db.session.commit()
+        return make_response(jsonify(message.to_dict()), 200)
+    
+    if request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return make_response(jsonify({"message": "Message successfully deleted"}), 200)
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
